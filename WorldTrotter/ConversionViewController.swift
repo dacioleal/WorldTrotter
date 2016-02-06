cd548d354260286aa8e13b772b76928367709276
dacioleal <dacioleal@gmail.com>
2016-02-06 18:32:29 +0100

    Conversion from Fahrenheit to celsius implemented

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -13,13 +13,38 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
+    var farenheitValue: Double? {
+        
+        didSet {
+            updateCelsiusValue()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        
+        if let value = farenheitValue {
+            return (value - 32) * (5 / 9)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NSNumberFormatter = {
+        
+       let nf = NSNumberFormatter()
+        nf.numberStyle = .DecimalStyle
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
     
     @IBAction func farenheitFieldEditingChanged(textField: UITextField) {
         
-        if let text = textField.text where !text.isEmpty {
-            celsiusLabel.text = text
+        if let text = textField.text, value = Double(text) {
+            farenheitValue = value
         } else {
-            celsiusLabel.text = "???"
+            farenheitValue = nil
         }
         
         
@@ -34,6 +59,14 @@ class ConversionViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func updateCelsiusValue() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.stringFromNumber(value)
+        } else {
+            celsiusLabel.text = "???"
+        }
     }
 
 
